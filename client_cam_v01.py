f7f785d294a26877447a2603044b5aab2f6ecf5a
guidovigiani <guidovigiani@gmail.com>
2020-10-18 20:12:03 -0300

    fix example

--- client_cam_v01.py
+++ client_cam_v01.py
@@ -5,7 +5,7 @@ import picamera
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 client_socket = socket.socket()
-client_socket.connect(('http://66.97.46.179', 3001))
+client_socket.connect(('127.0.0.1', 8000))
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
