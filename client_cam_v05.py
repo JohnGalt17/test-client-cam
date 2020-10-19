727732ab5d61bad34fe553c1bc9e7aae0e9325bc
guidovigiani <guidovigiani@gmail.com>
2020-10-18 23:22:53 -0300

    test

--- client_cam_v05.py
+++ client_cam_v05.py
@@ -1,45 +1,7 @@
-import io
-import socket
-import picamera
-import atexit
+# Camera1.py
+import camera
 
-def camServer():
-
-    while True:
-        print("wait...")
-        conn, addr = server_socket.accept()
-        if conn:
-            print(conn)
-            print(addr)
-            connection = conn.makefile('wb')
-            break
-
-    print("Connecting")
-    try:
-        stream = io.BytesIO()
-        camera.capture(stream, 'jpeg')
-        stream.seek(0)
-        connection.write(stream.read())
-        stream.seek(0)
-        stream.truncate()
-    finally:
-        print("close connection")
-        connection.close()
-
-def onExit():
-    connection.close()
-    server_socket.close()
-    print("exit")
-
-with picamera.PiCamera() as camera:
-    camera.resolution = (640, 480)
-    camera.start_preview()
-    atexit.register(onExit)
-
-    server_socket = socket.socket()
-    server_socket.bind(('66.97.46.179', 3003))
-    server_socket.listen(0)
-    server_socket.setblocking(1)
-    
-    while True:
-        camServer()
+print("Capturing image...")
+img = camera.captureJPEG(300, 200)
+camera.saveData(img, "/test.jpg")
+print "JPEG saved"
