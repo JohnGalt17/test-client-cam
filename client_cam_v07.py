9b0425fd6f37c1cf594e2bbe3ac490e9330a997a
guidovigiani <guidovigiani@gmail.com>
2020-10-18 23:38:57 -0300

    test

---
+++ client_cam_v07.py
@@ -0,0 +1,59 @@
+import io
+import time
+import picamera
+import socketio
+
+
+
+# standard Python
+soquete = socketio.Client()
+
+print("intentanto conectar")
+
+soquete.connect('http://66.97.46.179:3003/test')
+
+print('El sid es', soquete.sid)
+
+# Create an in-memory stream
+my_stream = io.BytesIO()
+with picamera.PiCamera() as camera:
+    camera.start_preview()
+    # Camera warm-up time
+    time.sleep(2)
+    camera.capture(my_stream, 'jpeg')
+    soquete.emit('imagen', camera)
+
+
+
+
+#soquete.emit('test', {'probando': 'desde python'})
+soquete.emit('test', 'ESTE MENSAJE VIENE DE PYTHON! ah y juan se la come')
+#soquete.emit()
+
+
+
+print("conectado")
+print("saliendo")
+
+
+soquete.disconnect()
+
+
+@soquete.event
+def message(data):
+    print('I received a message!')
+
+@soquete.event
+def connect():
+    print("I'm connected!")
+
+@soquete.event
+def connect_error():
+    print("The connection failed!")
+
+@soquete.event
+def disconnect():
+    print("I'm disconnected!")
+
+
+
