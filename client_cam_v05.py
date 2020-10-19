be6c5d4c86d1290666f47e538442206f7782732e
guidovigiani <guidovigiani@gmail.com>
2020-10-18 23:26:06 -0300

    fix

--- client_cam_v05.py
+++ client_cam_v05.py
@@ -1,7 +1,10 @@
-# Camera1.py
-import camera
+import time
+import picamera
+import numpy as np
 
-print("Capturing image...")
-img = camera.captureJPEG(300, 200)
-camera.saveData(img, "/test.jpg")
-print("JPEG saved")
+with picamera.PiCamera() as camera:
+    camera.resolution = (320, 240)
+    camera.framerate = 24
+    time.sleep(2)
+    output = np.empty((240, 320, 3), dtype=np.uint8)
+    camera.capture(output, 'rgb')
