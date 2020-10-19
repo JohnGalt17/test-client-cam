79f12be1e3127c1486f5f5de29d507d64786ce44
guidovigiani <guidovigiani@gmail.com>
2020-10-19 19:36:40 -0300

    test 09

---
+++ client_cam_v09.py
@@ -0,0 +1,60 @@
+import io
+import time
+import picamera
+import socketio
+
+
+
+# standard Python
+soquete = socketio.Client()
+
+print("intentanto conectar")
+
+soquete.connect('http://66.97.46.179:3003')
+
+print('El sid es', soquete.sid)
+
+print('Pruebo enviando un msj de test')
+soquete.emit('test', 'ESTE MENSAJE VIENE DE PYTHON! ah y juan se la come')
+
+print('obtengo imagen de la camera')
+import picamera
+camera = picamera.PiCamera()
+camera.start_preview()
+camera.capture("snapshot.jpg")
+camera.stop_preview()
+
+with open('snapshot.jpg', 'rb') as f:
+    image_data = f.read()
+
+print('Voy a mandar la imagen de la camera')
+soquete.emit('imagen', {'image_data': image_data})
+
+
+print('Pruebo enviando un msj de test 22')
+#soquete.emit('test', {'probando': 'desde python'})
+soquete.emit('test', 'ESTE MENSAJE VIENE DE PYTHON! ah y juan se la come')
+#soquete.emit()
+
+print("saliendo")
+soquete.disconnect()
+
+
+@soquete.event
+def message(data):
+    print('I received a message!')
+
+@soquete.event
+def connect():
+    print("I'm connected!")
+
+@soquete.event
+def connect_error():
+    print("The connection failed!")
+
+@soquete.event
+def disconnect():
+    print("I'm disconnected!")
+
+
+
